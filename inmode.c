nafmo/bastext

commit 1ce4c25e639ae1b0470b1fedd14a1c600ad14cfb

Actually Implement PET BASIC 4.0 support
The documentation mentions a "tokpet" header and 4.0 mode. Some code for
this was in the stash of forgotten code changes committed back in 1999,
but it wasn't activated or working.

Most of these commands are direct-mode only, so they are unlikely to be
used in actual binaries, so require "-b 4.0" to enable it when converting
to text.

There are also a couple of tokens here for a Commodore 64 BASIC 4.0
extension, which I do remember having somewhere, but cannot find now. The
one available through various on-line sources only have the regular 4.0
tokens, so I cannot verify them until I locate it.

inmode.c
@@ -215,6 +215,11 @@ void inconvert(FILE *input, FILE *output, const char *title, int adr,
 			}
 			fprintf(output, "\nstart tok128 %s\n", title);
 		}
+		else if (Basic4 == mode) {
+			/* tok64 doesn't handle BASIC 4 programs, so ignore strict mode */
+			strict = FALSE;
+			fprintf(output, "\nstart tokpet %s\n", title);
+		}
 		else if (X16 == mode) {
 			/* tok64 doesn't handle X16 programs, so ignore strict mode */
 			strict = FALSE;

bastext.1
@@ -147,6 +147,14 @@ interpretation of
 .I all
 programs.
 .TP
+.I \-b 4.0
+Force Commodore PET BASIC 4.0 interpretation of all programs.
+This must be specified both in input mode, as these tokens are incompatible
+with the 3.5 and 7.0 tokens. In output mode, the tokpe theader will enable
+Commodore PET 4.0 mode, but
+.I \-b 4.0
+must be specified if the text file uses a standard tok64 header.
+.TP
 .I \-b 7.0
 Force
 .I Commodore 128 BASIC 7.0
@@ -182,8 +190,9 @@ Enable Commander X16 BASIC support.
 This must be specified in input mode to properly detokenize Commander X16
 BASIC binary.
 In output mode, the tokx16 header will enable Commander X16 BASIC mode no
-matter which BASIC mode has been selected, but -x must be specifid if the
-text file uses a standard tok64 header.
+matter which BASIC mode has been selected, but
+.I \-b x16
+must be specified if the text file uses a standard tok64 header.
 .TP
 .I \-b Super
 Enable VIC-20 Super Expander support.

dtokeniz.c
@@ -172,6 +172,11 @@ int detokenize(const char *input_p, char *output_p, basic_t mode, int strict)
 					output_p += sprintf(output_p, "%s",
 					                    tfc3tokens[*ch_p - 204]);
 				} /* else */
+				else if (*ch_p <= 227 && Basic4 == mode) {
+					/* PET BASIC 4.0 */
+					output_p += sprintf(output_p, "%s",
+					                    basic4tokens[*ch_p - 204]);
+				} /* else */
 				else {
 					/* Errorneous token */
 					output_p += sprintf(output_p, "{%d}", *ch_p);

main.c
@@ -56,6 +56,7 @@ int main(int argc, char *argv[])
 	 *  b (basic) - force BASIC mode
 	 *    -b 2.0   - BASIC 2.0  (legacy: -2)
 	 *    -b 3.5   - BASIC 3.5
+	 *    -b 4.0   - BASIC 4.0
 	 *    -b 7.0   - BASIC 7.0  (legacy: -7)
 	 *    -b 7.1   - BASIC 7.1  (legacy: -1)
 	 *    -b 52    - Graphics53 (legacy: -5)
@@ -101,6 +102,9 @@ int main(int argc, char *argv[])
 				else if (0 == strcmp(optarg, "3.5")) {
 					force = Basic35;
 				}
+				else if (0 == strcmp(optarg, "4.0")) {
+					force = Basic4;
+				}
 				else if (0 == strcmp(optarg, "7.0")) {
 			case '7':
 					force = Basic7;
@@ -122,6 +126,10 @@ int main(int argc, char *argv[])
 				}
 				else if (0 == strcasecmp(optarg, "X16")) {
 					force = X16;
+				}
+				else {
+					fprintf(stderr, "Unrecognized BASIC dialect: %s\n", optarg);
+					return 1;
 				}
 				break;
 
@@ -183,18 +191,6 @@ int main(int argc, char *argv[])
 
 		switch (mode) {
 			case In:
-				switch (force) {
-					/* Only X16 and VIC-20 SuperExpander mode can be forced
-					 * in input mode; we might want to change that in the
-					 * future, though */
-					case X16:
-					case VicSuper:
-						break;
-
-					default:
-						force = Any;
-						break;
-				}
 				if (t64mode)	t642txt(argv[i], output, allfiles, strict, force);
 				else			bas2txt(argv[i], output, allfiles, strict, force);
 				break;

outmode.c
@@ -160,6 +160,16 @@ void txt2bas(const char *infile, basic_t force, outmode_t outmode)
 					if (Any == force)	mode = Basic71;
 				}
 			}
+			else if (strncasecmp(text, "start tokpet ", 13) == 0) {
+				/* This is the header that starts the actual BASIC text */
+				foundheader = TRUE;
+
+				/* Retrieve the file name */
+				strcpy(filename, &text[13]);
+
+				/* This is always a Commodore PET BASIC 4.0 progrem */
+				mode = Basic4;
+			}
 			else if (strncasecmp(text, "start tokx16 ", 13) == 0) {
 				/* This is the header that starts the actual BASIC text */
 				foundheader = TRUE;

select.c
@@ -17,10 +17,12 @@ basic_t selectbasic(int adr)
 	 * BASIC version
 	 *  0401 => BASIC 2.0 (VIC20, +3K RAM) or Graphics52 (C64)
 	 *          Graphics52 is the super-set, select it
+	 *          NB! PET BASIC 4.0 also uses 0401, but must
+	 *          use the -b 4.0 option.
 	 *  0801 => BASIC 2.0 (C64) or TFC3 BASIC (C64)
 	 *          TFC3 is the super-set, select it
 	 *          NB! Commander X16 BASIC also uses 0801, but
-	 *          must use the -x option.
+	 *          must use the -b X16 option.
 	 *  1001 => BASIC 2.0 (VIC20 unexpanded) or BASIC 3.5 (C16/Plus4)
 	 *          BASIC 3.5 is the superset
 	 *  1201 => BASIC 2.0 (VIC20 +8K RAM)

commit 6b80c10444213ebb6ff110d2cdffe867dd696990

inmode: Match footer with header
Fixes: 1ce4c25e639ae1b0 (Actually Implement PET BASIC 4.0 support)
Fixes: a186ecfb4334f00e (Disable Commander X16 BASIC mode by default)

inmode.c
@@ -278,9 +278,16 @@ void inconvert(FILE *input, FILE *output, const char *title, int adr,
 			fprintf(output, "63999 REM \"Invalid BASIC input %s\n", title);
 		}
 
-		/* Print tok64 footer */
+		/* Print tok64 footer; we only check for "stop tok" when reading
+		 * the file back, but match the start header above. */
 		if (Basic7 == mode || Basic71 == mode) {
 			fprintf(output, "stop tok128\n(" PROGNAME " " PROGVERSION ")\n");
+		}
+		else if (Basic4 == mode) {
+			fprintf(output, "stop tokpet\n(" PROGNAME " " PROGVERSION ")\n");
+		}
+		else if (X16 == mode) {
+			fprintf(output, "stop tokx16\n(" PROGNAME " " PROGVERSION ")\n");
 		}
 		else {
 			fprintf(output, "stop tok64\n(" PROGNAME " " PROGVERSION ")\n");