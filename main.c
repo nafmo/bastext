nafmo/bastext

commit f6691efc4000ec53176b5d623ab859c8984bea0a

Simplify command line
Having a single-letter option for each format and BASIC dialect worked
when we only supported a few, but we are running out of letters fast.

Replace BASIC selectors with "-b DIALECT" and format selector with "-f
FORMAT". Keep support for all the single-letter options from v1.1, but
remove mention of them from the documentation.

main.c
@@ -13,6 +13,10 @@
 #define TRUE 1
 #define FALSE 0
 
+#if defined __EMX__ || defined _MSC_VER
+#define strcasecmp stricmp
+#endif
+
 typedef enum runmode_e { None, In, Out } runmode_t;
 
 void helpscreen(const char *progname);
@@ -38,26 +42,35 @@ int main(int argc, char *argv[])
 	optswchar = SWITCH;
 #endif
 
-	/* Recognized options:
+	/* Operation modes:
 	 *  i (in)   - convert from binary to text
 	 *  o (out)  - convert from text to binary
-	 *  t (t64)  - T64 mode
-	 *  p (p00)  - P00 mode (out mode only; autodetected in in mode)
-	 *  2 (2.0)  - force BASIC 2.0        -\
-	 *  3 (TFC3) - force TFC3 BASIC         \
-	 *  5 (G52)  - force Graphics52 BASIC    >- out mode only,
-	 *  7 (7.0)  - force BASIC 7.0          /   if not specified, looks at
-	 *  1 (7.1)  - force BASIC 7.1        -/    "start bastext" header
-	 *  e (Exp)  - force VIC20 SuperExpander
-	 *  x (x16)  - force X16 BASIC (r48)
-	 *  a (all)  - convert all programs, not only those with recognized start
-	 *             address (0401/0801/1001/1201/132D/1C01/4001)
-	 *  s (strict)-strict tok64 encoding
-	 *  d (dest) - gives destination filename (followed by filename)
 	 *  h (help) - print help page
+	 *
+	 * File format:
+	 *  f (format) - select format for input/output file:
+	 *    -f t64 - T64 mode   (legacy: -t)
+	 *    -d p00 - P00 mode (out mode only; autodetecet in in mode)
+	 *
+	 * Source format:
+	 *  b (basic) - force BASIC mode
+	 *    -b 2.0   - BASIC 2.0  (legacy: -2)
+	 *    -b 3.5   - BASIC 3.5
+	 *    -b 7.0   - BASIC 7.0  (legacy: -7)
+	 *    -b 7.1   - BASIC 7.1  (legacy: -1)
+	 *    -b 52    - Graphics53 (legacy: -5)
+	 *    -b TFC3  - TFC 3      (legacy: -3)
+	 *    -b X16   - X16 BASIC
+	 *    -b Super - VIC-20 SuperExpander
+	 *
+	 * Modifiers:
+	 *  a (all)    - convert all programs, not only those with recognized start
+	 *               address (0401/0801/1001/1201/132D/1C01/4001)
+	 *  s (strict) - strict tok64 encoding
+	 *  d (dest)   - destination filename
 	 *  ? (help)
 	 */
-	while (-1 != (option = getopt(argc, argv, "iotp23571exasd:h?"))) {
+	while (-1 != (option = getopt(argc, argv, "iohf:b:asdt23571:?"))) {
 		switch (option) {
 			case 'i':
 				mode = In;
@@ -67,40 +80,49 @@ int main(int argc, char *argv[])
 				mode = Out;
 				break;
 
+			case 'f':
+				if (0 == strcasecmp(optarg, "t64")) {
 			case 't':
-				t64mode = TRUE;
+					t64mode = TRUE;
+					p00mode = FALSE;
+				}
+				else if (0 == strcasecmp(optarg, "p00")) {
+					t64mode = FALSE;
+					p00mode = TRUE;
+				}
 				break;
 
-			case 'p':
-				p00mode = TRUE;
-				break;
-
+			case 'b':
+				if (0 == strcmp(optarg, "2.0")) {
 			case '2':
-				force = Basic2;
-				break;
-
-			case '3':
-				force = TFC3;
-				break;
-
-			case '5':
-				force = Graphics52;
-				break;
-
+					force = Basic2;
+					break;
+				}
+				else if (0 == strcmp(optarg, "3.5")) {
+					force = Basic35;
+				}
+				else if (0 == strcmp(optarg, "7.0")) {
 			case '7':
-				force = Basic7;
-				break;
-
+					force = Basic7;
+				}
+				else if (0 == strcmp(optarg, "7.1")) {
 			case '1':
-				force = Basic71;
-				break;
-
-			case 'e':
-				force = VicSuper;
-				break;
-
-			case 'x':
-				force = X16;
+					force = Basic71;
+				}
+				else if (0 == strcmp(optarg, "52")) {
+			case '5':
+					force = Graphics52;
+				}
+				else if (0 == strcasecmp(optarg, "TFC3")) {
+			case '3':
+					force = TFC3;
+				}
+				else if (0 == strcasecmp(optarg, "Super")) {
+					force = VicSuper;
+				}
+				else if (0 == strcasecmp(optarg, "X16")) {
+					force = X16;
+				}
 				break;
 
 			case 'a':
@@ -195,31 +217,30 @@ void helpscreen(const char *progname)
 {
 	fprintf(stderr,
 	        "Usage:\n"
-	        "  %s " SWITCH "i [" SWITCH "t] [" SWITCH "e|" SWITCH "x] [" SWITCH "a] [" SWITCH "s] [" SWITCH "d filename] filename(s)\n"
-	        "  %s " SWITCH "o [" SWITCH "t|" SWITCH "p] [" SWITCH "2|" SWITCH "3|" SWITCH "4|" SWITCH "5|" SWITCH "7|" SWITCH "1|" SWITCH "e|" SWITCH "x] filename(s)\n"
+	        "  %s " SWITCH "i|" SWITCH "o [" SWITCH "f TYPE] [" SWITCH "b MODE] [" SWITCH "a] [" SWITCH "s] [" SWITCH "d filename] filename(s)\n"
 	        "  %s " SWITCH "h\n"
 	        "\n Mode (one of these required):\n"
 	        "  " SWITCH "i\tInput mode (binary to text)\n"
 	        "  " SWITCH "o\tOutput mode (text to binary)\n"
 	        "  " SWITCH "h\tPrint help page\n"
-	        "\n General modfiers:\n"
-	        "  " SWITCH "t\tT64 mode (in: reads from specified T64 archive(s)\n"
+	        "\n File format selection:\n"
+	        "  " SWITCH "ft64\tT64 mode (in: reads from specified T64 archive(s)\n"
 	        "    \t          out: creates/appends to bastext.t64)\n"
-	        "  " SWITCH "x\tEnable Commander X16 BASIC support\n"
-	        "  " SWITCH "e\tForce VIC-20 SuperExpander BASIC interpretation\n"
-	        "\n Input mode modfiers:\n"
+	        "  " SWITCH "fp00\tP00 mode (out: wraps output in .p00 container)\n"
+	        "\n BASIC dialect selection:\n"
+	        "  " SWITCH "b2.0\tForce C64 BASIC 2.0 interpretation\n"
+	        "  " SWITCH "b3.5\tForce C16/+4 BASIC 3.5 interpretation\n"
+	        "  " SWITCH "b7.0\tForce C128 BASIC 7.0 interpretation\n"
+	        "  " SWITCH "b7.1\tForce C128 BASIC 7.1 interpretation\n"
+	        "  " SWITCH "bTFC3\n\tForce C64 TFC3 interpretation\n"
+	        "  " SWITCH "b52\tForce C64 Graphics52 interpretation\n"
+	        "  " SWITCH "bX16\tEnable Commander X16 BASIC support\n"
+	        "  " SWITCH "bSuper\n\tForce VIC-20 SuperExpander BASIC interpretation\n"
+	        "\n Behavioural options:\n"
 	        "  " SWITCH "a\tConvert all, not just recognized start addresses\n"
 	        "    \t (0401/0801/1001/1201/132D/1C01/4001)\n"
 	        "  " SWITCH "s\tStrict tok64 compatibility\n"
-	        "  " SWITCH "d fn\tSend output to file fn\n"
-	        "\n Output mode modifiers:\n"
-	        "  " SWITCH "p\tP00 mode (out: wraps output in .p00 container)\n"
-	        "  " SWITCH "2\tForce C64 BASIC 2.0 interpretation\n"
-	        "  " SWITCH "3\tForce C64 TFC3 interpretation\n"
-	        "  " SWITCH "5\tForce C64 Graphics52 interpretation\n"
-	        "  " SWITCH "7\tForce C128 BASIC 7.0 interpretation\n"
-	        "  " SWITCH "1\tForce C128 BASIC 7.1 interpretation\n",
-	        progname,
+	        "  " SWITCH "d fn\tSend output to file fn\n",
 	        progname,
 	        progname);
 }

bastext.1
@@ -4,13 +4,11 @@ bastext \- convert Commodore BASIC to text
 .SH SYNOPSIS
 .PD 0
 .B bastext
-\-i [\-t] [\-a] [\-s] [\-d filename]
-filename(s)
-.PP
-.B bastext
-\-o
-[\-t] [\-2|\-3|\-5|\-7|\-1]
-filename(s)
+\-i|\-o [\-f
+.IR TYPE ]
+[\-b
+.IR MODE ]
+[\-a] [\-s] [\-d filename] filename(s)
 .PP
 .B bastext
 \-h
@@ -69,6 +67,7 @@ written in hexadecimal):
 .TP
 1201
 .I VIC-20 BASIC 2.0 (8K RAM expansion)
+.TP
 132D
 .IR "Commodore 128 BASIC 7.1 extension by Rick Simon" .
 This file is a combined file, with both the BASIC 7.1
@@ -78,6 +77,7 @@ with BASIC 7.1's
 command).
 The preamble will be ignored, and the file will be
 interpreted as BASIC 7.1.
+.TP
 1C01
 .I Commodore 128 BASIC 7.0
 .TP
@@ -97,33 +97,18 @@ BASIC).
 .TP
 .I \-h
 Shows a brief help screen, with an overview of the available options.
-.SS "GENERAL MODIFIERS"
-.PP
-These general modifiers (works in both input and output modes)
-are available:
+.SS "FORMAT SELECTION"
+The
+.I \-f
+option selects the format of the input and output files:
 .TP
-.I \-t
+.I \-f t64
 Enable T64 (Commodore 64 emulator tape archive) mode.
 When in input mode, this means that instead of the specified file
 names being binary Commodore BASIC files, they are T64 archives.
 When in output mode, this means that instead of writing the
 binary Commodore BASIC files to files in the current directory,
 they will be written to a T64 archive named
-.TP
-.I \-x
-Enable Commander X16 BASIC support.
-This must be specified in input mode to properly detokenize Commander X16
-BASIC binary.
-In output mode, the tokx16 header will enable Commander X16 BASIC mode no
-matter which BASIC mode has been selected, but -x must be specifid if the
-text file uses a standard tok64 header.
-.TP
-.I \-e
-Enable VIC-20 Super Expander support.
-This must be specified both in input and output mode, as bastext for
-compatibility reasons selects the Commodore 64 Graphics52 expansion by
-default.
-
 .B bastext.t64
 in the current directory.
 If the archive already exists, it will be appended to.
@@ -135,13 +120,81 @@ an error message.
 The default directory size is controlled in the
 .I t64.h
 file.
-.SS "INPUT MODE MODIFIERS"
-.PP
-These modifiers are available only when in input mode:
+.TP
+.I \-f p00
+Enable P00 (PC64 emulator container file) mode.
+The Commodore BASIC files are written to the current directory but get a
+PC64 container header written to them and a file extension of .p00 added.
+This parameter is ignored in input mode, as it will auto-detect the .p00
+container format.
+.SS "BASIC SELECTION"
+The
+.I \-b
+option selects the BASIC dialect to use; if not specified,
+the format is detected automatically, unless otherwise mentioned:
+.TP
+.I \-b 2.0
+Force
+.I Commodore BASIC 2.0
+interpretation of
+.I all
+programs.
+.TP
+.I \-b 3.5
+Force
+.I Commodore C16/+4 BASIC 3.5
+interpretation of
+.I all
+programs.
+.TP
+.I \-b 7.0
+Force
+.I Commodore 128 BASIC 7.0
+interpretation of
+.I all
+programs.
+.TP
+.I \-b 7.1
+Force
+.I Commodore 128 BASIC 7.1 extension
+interpretation of
+.I all
+programs.
+.TP
+.I \-b TFC3
+Force
+.I Commodore 64 The Final Cartridge III BASIC extension
+interpretation of
+.I all
+programs.
+(See also
+.BR BUGS ).
+.TP
+.I \-b 52
+Force
+.I Commodore 64 Graphics52 BASIC extension
+interpretation of
+.I all
+programs.
+.TP
+.I \-b X16
+Enable Commander X16 BASIC support.
+This must be specified in input mode to properly detokenize Commander X16
+BASIC binary.
+In output mode, the tokx16 header will enable Commander X16 BASIC mode no
+matter which BASIC mode has been selected, but -x must be specifid if the
+text file uses a standard tok64 header.
+.TP
+.I \-b Super
+Enable VIC-20 Super Expander support.
+This must be specified both in input and output mode, as bastext for
+compatibility reasons selects the Commodore 64 Graphics52 expansion by
+default.
+.SS "BEHAVIOURAL OPTIONS"
 .TP
 .I \-a
 Convert all input files, not only those that have a "recognized"
-starting address.
+starting address (input mode only).
 .TP
 .I \-s
 Maintain strict compatibility with
@@ -158,58 +211,11 @@ The "strict" mode will not, however, undo the problems with
 "uppercase in quoted strings"-bug (see under
 .BR BUGS ).
 .TP
-.I \-d filename
+.IR \-d \ filename
 Selects the filename to write the output to.
 If the filename is not given, or is given as "-", the listings
 will be output on the standard output device (normally the
 console).
-.SS "OUTPUT MODE MODIFIERS"
-.PP
-These modifiers are available only when in output mode:
-.TP
-.I \-p
-Enable P00 (PC64 emulator container file) mode.
-The Commodore BASIC files are written to the current directory but get a
-PC64 container header written to them and a file extension of .p00 added.
-This parameter is ignored in input mode, as it will auto-detect the .p00
-container format.
-.TP
-.I \-2
-Force
-.I Commodore BASIC 2.0
-interpretation of
-.I all
-programs.
-.TP
-.I \-3
-Force
-.I Commodore 64 The Final Cartridge III BASIC extension
-interpretation of
-.I all
-programs.
-(See also
-.BR BUGS ).
-.TP
-.I \-5
-Force
-.I Commodore 64 Graphics52 BASIC extension
-interpretation of
-.I all
-programs.
-.TP
-.I \-7
-Force
-.I Commodore 128 BASIC 7.0
-interpretation of
-.I all
-programs.
-.TP
-.I \-1
-Force
-.I Commodore 128 BASIC 7.1 extension
-interpretation of
-.I all
-programs.
 .PP
 Please note that the MS-DOS and OS/2 versions (EMX compiled)
 uses
@@ -231,13 +237,13 @@ in the current directory, while maintaining
 .B tok64
 compatibility.
 .TP
-.B bastext \-it *.t64 | more
+.B bastext \-ift64 *.t64 | more
 Converts all files in all T64 archives (with filename suffix
 .IR .t64 )
 in the current directory into listings, displaying them
 one page at a time.
 .TP
-.B bastext \-o7 programs.txt
+.B bastext \-ob7.0 programs.txt
 Converts all programs in the
 .I programs.txt
 text file into Commodore BASIC 7.0 programs.
@@ -273,12 +279,12 @@ outputs (without the
 .I start tok64
 header).
 .SH VERSION\ INFORMATION
-This documentation covers version 1.1 of
+This documentation covers version 1.2 of
 .BR BasText .
 .SH AUTHOR
 BASTEXT was written by Peter Krefting.
 If you have bug-reports or questions, mail them to peter@softwolves.pp.se
 .PP
-A Softwolves Software (tm) Release in 2025.
+A Softwolves Software (tm) Release in 2026.
 .PP
-This program is released under the GNU Public License.
+This program is released under the GNU General Public License version 2.

commit 0204b10405347ef15ddabcff2d47bc2c7f7ad115

main: Fix -d option
Add missing ":" to make sure the -d option always has an argument.

Fixes: f6691efc4000ec53 (Simplify command line)

main.c
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
 	 *  d (dest)   - destination filename
 	 *  ? (help)
 	 */
-	while (-1 != (option = getopt(argc, argv, "iohf:b:asdt23571:?"))) {
+	while (-1 != (option = getopt(argc, argv, "iohf:b:asd:t23571:?"))) {
 		switch (option) {
 			case 'i':
 				mode = In;