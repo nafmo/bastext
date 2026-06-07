nafmo/bastext

commit 811c9e6f563cc3165e8140560cd08201c6a89c4b

Allow reading files from D64 images
Add support for reading D64 files; borrows the D64 reading code from
d64-browser.

Since inconvert() takes a FILE and reads directly from that, we must
first extract the file from the D64 before feeding it in (GNU libc
has fopencookie that could allow us to work around that, but that is
not widely available).

Use tmpfile() for now (which requires C11 support). According to the
documentation, the Microsoft implementation of this tries to write
its files to the root of drive C: (!), so this needs to be rewritten.

inmode.c
@@ -10,8 +10,10 @@
 #include "version.h"
 #include "select.h"
 #include "t64.h"
+#include "d64.h"
 
 void inconvert(FILE *, FILE *, const char *, int, bool, bool, basic_t);
+void from_petscii_name(char title[21], char petscii_filename[16]);
 
 /* bas2txt
  * - converts a binary file into a text file
@@ -92,7 +94,7 @@ void bas2txt(const char *infile, FILE *output, bool allfiles, bool strict, basic
 void t642txt(const char *infile, FILE *output, bool allfiles, bool strict, basic_t force)
 {
 	FILE			*input;
-	char			title[21], *c_p;
+	char			title[21];
 	t64header_t		header;
 	t64record_t		record;
 	unsigned int	totalentries, usedentries;
@@ -126,30 +128,7 @@ void t642txt(const char *infile, FILE *output, bool allfiles, bool strict, basic
 			/* This is an allocated entry, with a normal program file in it */
 
 			/* Get the file title */
-			strncpy(title, record.filename, 16);
-			title[16] = 0;		/* null terminate */
-
-			while ((char) 32 == title[strlen(title) - 1] ||
-			       (char) 160 == title[strlen(title) - 1]) {
-				/* Remove trailing spaces */
-				title[strlen(title) - 1] = 0;
-			}
-
-			/* Convert to uppercase ASCII, and change spaces to underscores */
-			c_p = title;
-			while (*c_p) {
-				*c_p &= 0x7F;			/* Strip highbit */
-				if (0x60 == (*c_p & 0x60)) {
-					*c_p &= ~0x20;		/* Lowercase => uppercase */
-				}
-				else if (' ' == *c_p) {
-					*c_p = '_';
-				}
-				c_p ++;
-			}
-
-			/* Add .prg suffix */
-			strcat(title, ".prg");
+			from_petscii_name(title, record.filename);
 
 			/* Retrieve the starting address */
 			adr = record.startaddress[0] | (record.startaddress[1] << 8);
@@ -167,6 +146,155 @@ void t642txt(const char *infile, FILE *output, bool allfiles, bool strict, basic
 
 	/* Close files */
 	fclose(input);
+}
+
+void d642txt(const char *infile, FILE *output, bool allfiles, bool strict, basic_t force)
+{
+	FILE			*input, *prgfile;
+	uint8_t			sector;
+	bam_t			bam;
+	dirblock_t		dirblock;
+	char			title[21];
+	uint8_t			buf[256];       /* One disk sector */
+
+	/* First, open input file */
+	input = opend64(infile, &bam);
+	if (!input) {
+		fprintf(stderr, "Unable to open input file: %s\n", infile);
+		exit(1);
+	}
+
+	/* Read the directory; we ignore the BAM completely */
+	sector = 1;
+	while (sector) {
+		/* Load directory block */
+		fseek(input, 91392 + sector * 256, SEEK_SET);
+		fread(&dirblock, sizeof (dirblock), 1, input);
+
+		/* Each block contains eight directory entries */
+		for (size_t i = 0; i < 8; ++ i) {
+			if ((dirblock.file[i].filetype & (D64_CLO | D64_FTY)) == (D64_PRG | D64_CLO)) {
+				/* This is a valid PRG file entry */
+				int				adr;
+				size_t			blocks;
+				uint8_t			t, s;
+				bool			valid;
+
+				/* Get the file title */
+				from_petscii_name(title, dirblock.file[i].name);
+
+				/* inconvert() requires a linear file. Copy the contents to a
+				 * temporary file, which we then feed it.
+				 *
+				 * While tmpfile() is portable (C11), it seems to always write
+				 * to /tmp (GNU libc) or the root directory (Microsoft), which
+				 * is *bad*. Reconsider this API.
+				 *
+				 * For GNU libc, consider using fopencookie() to avoid the
+				 * temporary file altogether, or rewrite inconvert() to take
+				 * a buffer instead.
+				 */
+				prgfile = tmpfile();
+				if (!prgfile) {
+					fprintf(stderr, "Unable to allocate temporary file for %s\n", title);
+					continue;
+				}
+
+				/* Check starting position for sanity */
+				t = dirblock.file[i].t;
+				s = dirblock.file[i].s;
+				if (t == 18 || t > 35 || s > 21) {
+					fprintf(stderr, "Directory entry for %s is invalid\n", title);
+					continue;
+				}
+
+				/* Make sure loops don't kill us */
+				blocks = 664;
+
+				/* End of file marker is track == 0 */
+				valid = true;
+				while (t && blocks --) {
+					/* Read requested sector */
+					fseek(input, ts2block(t, s) * 256, SEEK_SET);
+					fread(buf, 1, 256, input);
+
+					/* Read links */
+					t = buf[0];
+					s = buf[1]; /* length of data if t == 0 */
+
+					/* Write to the temporary file */
+					fwrite(&buf[2], 1, t ? 254 : s, prgfile);
+
+					/* Check for error conditions */
+					if (t && (t == 18 || t > 35 || s > 21)) {
+						fprintf(stderr, "File %s contains invalid track/sector links [%d:%d]\n", title, t, s);
+						valid = false;
+						t = 0;
+					}
+
+				}
+
+				if (valid) {
+					/* Now rewind the file to read the start address */
+					fseek(prgfile, 0, SEEK_SET);
+					adr = fgetc(prgfile);		/* low byte */
+					adr |= fgetc(prgfile) << 8;	/* high byte */
+
+					/* Now convert the file to text */
+					inconvert(prgfile, output, title, adr, allfiles, strict, force);
+				}
+
+				/* Closing the temporary files deletes it */
+				fclose(prgfile);
+			}
+		}
+
+		/* Follow the directory links, making sure we stay on the right
+		 * track. */
+		sector = dirblock.link.s;
+		if (dirblock.link.t != 18) sector = 0;
+	}
+
+	/* Finish up */
+	fclose(input);
+}
+
+/* Copy filename from the T64, D64 or P00 record and convert
+ * to ASCII.
+ *
+ * filename is always 16 characters long, but can be padded
+ * with nulls, spaces (0x20) or shifted spaces (0xA0).
+ *
+ * The output buffer must be 21 characters long as we add a
+ * ".prg" suffix and a terminating null character */
+void from_petscii_name(char title[21], char petscii_filename[16])
+{
+	char	*c_p;
+
+	memcpy(title, petscii_filename, 16);
+	title[16] = 0;		/* null terminate */
+
+	while ((char) 32 == title[strlen(title) - 1] ||
+	       (char) 160 == title[strlen(title) - 1]) {
+		/* Remove trailing spaces */
+		title[strlen(title) - 1] = 0;
+	}
+
+	/* Convert to uppercase ASCII, and change spaces to underscores */
+	c_p = title;
+	while (*c_p) {
+		*c_p &= 0x7F;			/* Strip highbit */
+		if (0x60 == (*c_p & 0x60)) {
+			*c_p &= ~0x20;		/* Lowercase => uppercase */
+		}
+		else if (' ' == *c_p) {
+			*c_p = '_';
+		}
+		c_p ++;
+	}
+
+	/* Add .prg suffix */
+	strcat(title, ".prg");
 }
 
 /* inconvert

Makefile
@@ -1,5 +1,5 @@
 # Makefile for bastext -------------------------------------------------------
-OBJS=main.o inmode.o outmode.o tokens.o tokenize.o dtokeniz.o select.o t64.o utility.o
+OBJS=main.o inmode.o outmode.o tokens.o tokenize.o dtokeniz.o select.o t64.o d64.o utility.o
 CFLAGS=-Wall
 
 # All targets ----------------------------------------------------------------
@@ -13,10 +13,11 @@ tokens.o: tokens.c tokens.h
 tokenize.o: tokenize.c tokenize.h tokens.h
 dtokeniz.o: dtokeniz.c tokenize.h tokens.h
 main.o: main.c inmode.h outmode.h tokenize.h
-inmode.o: inmode.c tokenize.h version.h inmode.h select.h t64.h
+inmode.o: inmode.c tokenize.h version.h inmode.h select.h t64.h d64.h
 outmode.o: outmode.c tokenize.h version.h outmode.h select.h t64.h
 select.o: select.c select.h tokenize.h
 t64.o: t64.c t64.h
+d64.o: d64.c d64.h
 utility.o: utility.c utility.h
 
 # Cleanup --------------------------------------------------------------------

NMAKEFILE
@@ -1,5 +1,5 @@
 # Makefile for bastext for Windows -------------------------------------------
-OBJS=main.obj inmode.obj outmode.obj tokens.obj tokenize.obj dtokeniz.obj select.obj t64.obj utility.obj
+OBJS=main.obj inmode.obj outmode.obj tokens.obj tokenize.obj dtokeniz.obj select.obj t64.obj d64.obj utility.obj
 CFLAGS=/Wall /D_CRT_SECURE_NO_WARNINGS
 RM=del
 
@@ -14,10 +14,11 @@ tokens.obj: tokens.c tokens.h
 tokenize.obj: tokenize.c tokenize.h tokens.h
 dtokeniz.obj: dtokeniz.c tokenize.h tokens.h
 main.obj: main.c inmode.h outmode.h tokenize.h
-inmode.obj: inmode.c tokenize.h version.h inmode.h select.h t64.h
+inmode.obj: inmode.c tokenize.h version.h inmode.h select.h t64.h d64.h
 outmode.obj: outmode.c tokenize.h version.h outmode.h select.h t64.h
 select.obj: select.c select.h tokenize.h
 t64.obj: t64.c t64.h
+d64.obj: d64.c d64.h
 utility.obj: utility.c utility.h
 
 # Cleanup --------------------------------------------------------------------

bastext.1
@@ -121,6 +121,12 @@ The default directory size is controlled in the
 .I t64.h
 file.
 .TP
+.I \-f d64
+Enable D64 (Commodore 1541 disk image file) mode.
+When in input mode, this means that instead of the specified file
+names being binary Commodore BASIC files, they are D64 images.
+Ignored in output mode.
+.TP
 .I \-f p00
 Enable P00 (PC64 emulator container file) mode.
 The Commodore BASIC files are written to the current directory but get a

d64.c
@@ -1,0 +1,54 @@
+#include "d64.h"
+#include <string.h>
+
+size_t ts2block(uint8_t track, uint8_t sector)
+{
+    if (track < 18) return ((size_t) track -  1) * 21 + sector;
+    if (track < 25) return ((size_t) track - 18) * 19 + 17 * 21 + sector;
+    if (track < 31) return ((size_t) track - 25) * 18 + 17 * 21 + 19 * 7 + sector;
+    return ((size_t) track - 21) * 17 + 17 * 21 + 19 * 7 + 18 * 6 + sector;
+}
+
+FILE *opend64(const char *fname, bam_t *bam_p)
+{
+    FILE *f;
+
+    memset(bam_p, 0, sizeof (bam_t));
+
+    /* Open the file */
+    f = fopen(fname, "r");
+    if (!f) {
+        /* Unable to open file */
+        fprintf(stderr, "Unable to open %s for reading\n", fname);
+        return NULL;
+    }
+
+    /* Check size */
+    fseek(f, 0, SEEK_END);
+    if (ftell(f) != 174848) {
+        /* File size mismatch */
+        fprintf(stderr, "D64 image %s size mismatch\n", fname);
+        goto out;
+    }
+
+    /* Check integrity */
+    fseek(f, 91392, SEEK_SET); /* Start of directory header block */
+    fread(bam_p, sizeof (bam_t), 1, f);
+    if (bam_p->format != 'A') {
+        /* Something is strange here */
+        fprintf(stderr, "D64 image %s is not a C1541 disk image\n", fname);
+        goto out;
+    }
+    if (bam_p->t != 18 || bam_p->s != 1) {
+        /* That is strange, but ignored by a real drive */
+        fprintf(stderr, "D64 image %s has non-standard directory start "
+                "(%d,%d); assuming 18:1\n", fname, bam_p->t, bam_p->s);
+    }
+
+    /* Looks fine to us */
+    return f;
+
+out:
+    fclose(f);
+    return NULL;
+}

d64.h
@@ -1,0 +1,84 @@
+#pragma once
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+/* Structure definitions for D64 format, i.e. a byte-for-byte image of
+ * a Commodore 1541 disk.
+ */
+
+#pragma pack(push,1)
+
+/* BAM location */
+#define BAM_TRACK   18
+#define BAM_SECTOR  0
+
+/* Directory location */
+#define DIR_TRACK   18
+#define DIR_SECTOR  1
+
+/* Sector interleave */
+#define D64_INTERLEAVE 3
+
+/* Structure for BAM (18,0) */
+typedef struct bam_s
+{
+	uint8_t t, s;           /* 00-01: Track and sector of next block (18, 1) */
+	uint8_t format;         /*    02: DOS version type (0x41) */
+	uint8_t unused1;        /*    03: Unusued */
+	uint8_t bitmap[140];    /* 04-8F: BAM entries, four bytes per track */
+	char    diskname[16];   /* 90-9F: Disk name (PETSCII, padded with 0xA0) */
+	uint8_t fill1[2];       /* A0-A1: Filled with 0xA0 */
+	uint8_t id[5];          /* A2-A6: 2 char Disk ID, 0xA0, DOS type, but can be anything */
+	uint8_t fill2[4];       /* A7-AA: Filled with 0xA0 */
+	uint8_t unused2[86];    /* AB-FF: Unused (0x00), used by 40-track extensions or GEOS */
+} bam_t;
+
+/* Structure for directory entries */
+typedef struct dirent_s
+{
+	uint8_t filler[2];      /* 00-01: Track and sector of next block (first entry only) */
+	uint8_t filetype;       /*    02: File type */
+	uint8_t t, s;           /* 03-04: Track and sector of first sector in file */
+	char    name[16];       /* 05-14: Name of file (PETSCII, padded with 0xA0) */
+	uint8_t st, ss;         /* 15-16: First side-sector block (REL); GEOS: info sector */
+	uint8_t rlen;           /*    17: Length for REL file records; GEOS: file structure */
+	uint8_t geos_filetype;  /*    18: GEOS: filetype (0x00 for normal C64 file) */
+	uint8_t year, month, day;/*19-1B: GEOS: Year - 1900, month, day */
+	uint8_t hour, minute;   /* 1C-1D: GEOS: Hour, minute. Also @SAVE temporary storage */
+	uint8_t length[2];      /* 1E-1F: File size in sectors */
+} dirent_t;
+
+/* Structure for directory blocks (chain starting at 18,1) */
+typedef union dirblock_u
+{
+	dirent_t file[8];       /* 00-FF: Eight directory entries */
+	struct
+	{
+		uint8_t t, s;       /* 00-01: Track and sector of next block */
+	} link;
+} dirblock_t;
+
+/* File types for filetype field; ORed together */
+typedef enum d64_filetype_e {
+	/* Bits 0-3: file types */
+	D64_DEL = 0x00,     /* Deleted file, with close flag, shows as an unloadable "DEL" file */
+	D64_SEQ = 0x01,     /* Sequential file */
+	D64_PRG = 0x02,     /* Program file */
+	D64_USR = 0x03,     /* User-defined file */
+	D64_REL = 0x04,     /* Relative file */
+	D64_FTY = 0x07,     /* File type mask */
+	/* Bit 6: Lock flag */
+	D64_LCK = 0x40,
+	/* Bit 7: Close flag; if not set, indicates a splat file */
+	D64_CLO = 0x80
+} d64_filetype_t;
+
+#pragma pack(pop)
+
+/* Convert combination of track and sector number to a linear block number
+ * which is easier to use in D64 files (multiply by 256 for position). */
+size_t ts2block(uint8_t track, uint8_t sector);
+
+/* Open a D64 with checking */
+FILE *opend64(const char *fname, bam_t *bam_p);

inmode.h
@@ -4,3 +4,4 @@
 
 void bas2txt(const char *infile, FILE *output, bool allfiles, bool strict, basic_t force);
 void t642txt(const char *infile, FILE *output, bool allfiles, bool strict, basic_t force);
+void d642txt(const char *infile, FILE *output, bool allfiles, bool strict, basic_t force);

main.c
@@ -29,8 +29,7 @@ int main(int argc, char *argv[])
 {
 	int			option;
 	bool		allfiles = false;
-	bool		t64mode = false;
-	bool		p00mode = false;
+	outmode_t	outmode = Prg;
 	bool		strict = false;
 	runmode_t	mode = None;
 	basic_t		force = Any;
@@ -50,7 +49,8 @@ int main(int argc, char *argv[])
 	 * File format:
 	 *  f (format) - select format for input/output file:
 	 *    -f t64 - T64 mode   (legacy: -t)
-	 *    -d p00 - P00 mode (out mode only; autodetecet in in mode)
+	 *    -f d64 - D64 mode (out mode only)
+	 *    -f p00 - P00 mode (out mode only; autodetecet in in mode)
 	 *
 	 * Source format:
 	 *  b (basic) - force BASIC mode
@@ -84,12 +84,13 @@ int main(int argc, char *argv[])
 			case 'f':
 				if (0 == strcasecmp(optarg, "t64")) {
 			case 't':
-					t64mode = true;
-					p00mode = false;
+					outmode = T64;
+				}
+				else if (0 == strcasecmp(optarg, "d64")) {
+					outmode = D64;
 				}
 				else if (0 == strcasecmp(optarg, "p00")) {
-					t64mode = false;
-					p00mode = true;
+					outmode = P00;
 				}
 				break;
 
@@ -197,12 +198,23 @@ int main(int argc, char *argv[])
 
 		switch (mode) {
 			case In:
-				if (t64mode)	t642txt(argv[i], output, allfiles, strict, force);
-				else			bas2txt(argv[i], output, allfiles, strict, force);
+				switch (outmode) {
+					case T64:
+						t642txt(argv[i], output, allfiles, strict, force);
+						break;
+
+					case D64:
+						d642txt(argv[i], output, allfiles, strict, force);
+						break;
+
+					default:
+						bas2txt(argv[i], output, allfiles, strict, force);
+						break;
+				}
 				break;
 
 			case Out:
-				txt2bas(argv[i], force, t64mode ? T64 : (p00mode ? P00 : Prg));
+				txt2bas(argv[i], force, outmode);
 				break;
 
 		case None:
@@ -229,6 +241,7 @@ void helpscreen(const char *progname)
 	        "  " SWITCH "ft64\tT64 mode (in: reads from specified T64 archive(s)\n"
 	        "    \t          out: creates/appends to bastext.t64)\n"
 	        "  " SWITCH "fp00\tP00 mode (out: wraps output in .p00 container)\n"
+	        "  " SWITCH "fd64\tD64 mode (in: reads from specifiied D64 image(s))\n"
 	        "\n BASIC dialect selection:\n"
 	        "  " SWITCH "b2.0\tForce C64 BASIC 2.0 interpretation\n"
 	        "  " SWITCH "b3.5\tForce C16/+4 BASIC 3.5 interpretation\n"

commit ee614c6b89ce365043570c2150457712633fb10b

inmode: Call from_petscii_name() on P00 names
When I broke out the from_petscii_name() function from the T64 reader for
use in the D64 reader, I also intended to use it for P00 files, as they
are also encoded in PETSCII, but I forgot to do so.

Fixes: 811c9e6f563cc316 (Allow reading files from D64 images)
Fixes: 2ad94e3620dedcf9 (Add support for reading P00 files)

inmode.c
@@ -35,7 +35,7 @@ void bas2txt(const char *infile, FILE *output, bool allfiles, bool strict, basic
 {
 	FILE		*input;
 	const char	*title_p;
-	char        title_buf[17];
+	char		title_buf[21];
 	int			adr;
 
 	/* First, open input file */
@@ -65,6 +65,8 @@ void bas2txt(const char *infile, FILE *output, bool allfiles, bool strict, basic
 	/* Check for P00 file signature - "C64File\0" */
 	if (0x3643 == adr)
 	{
+		char	buf[16];
+
 		if (fgetc(input) != '4' ||
 		    fgetc(input) != 'F' ||
 		    fgetc(input) != 'i' ||
@@ -78,8 +80,9 @@ void bas2txt(const char *infile, FILE *output, bool allfiles, bool strict, basic
 		}
 
 		/* Read the PETSCII file name and point to it */
-		memset(title_buf, 0, sizeof(title_buf));
-		fread(title_buf, 1, 16, input);
+		memset(buf, 0, sizeof(buf));
+		fread(buf, 1, 16, input);
+		from_petscii_name(title_buf, buf);
 		title_p = title_buf;
 
 		/* Ignore nul and REL metadata */